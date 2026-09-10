745335a8aa407990a9118de09d75333fc28913dd
duckduckdoof <duckduckdoof@gmail.com>
2026-09-10 14:37:36 -0400

    Added epsilon scheduling for actor

--- modules/actor.py
+++ modules/actor.py
@@ -10,9 +10,63 @@ Actor for the atari game environment.
 """
 
 # IMPORTS
+import gymnasium as gym
+import numpy as np
+
+import torch
+from torch import nn
 
 # CONSTANTS
 
 # FUNCTIONS
+def greedy_epsilon(q_net: nn.Module, env: gym.Env, state: torch.Tensor, epsilon: float, device):
+    """
+    Performs epsilon-greedy selection of action (exploration/exploitation tradeoff).
+    """
+    if epsilon > np.random.uniform():
+        return env.action_space.sample()
+    else:
+        with torch.no_grad():
+            state = state.float().div_(255.).to(device)
+            q_values = q_net(state)
+            predicted_actions = q_values.argmax(dim=1).cpu().numpy()
+            return predicted_actions
 
 # CLASSES
+class EpsilonScheduler:
+    """
+    This decays epsilon at a linear rate by the number of steps past a
+    defined threshold.
+
+    Credit: https://www.kaggle.com/code/shreydan/deep-q-learning-playing-atari-breakout#Double-DQN-(DDQN)
+    """
+
+    def __init__(
+        self,
+        eps_min: float,
+        eps_max: float,
+        steps: int,
+        exploration_ratio: float = 0.1
+    ):
+        self.eps_min = eps_min
+        self.eps_max = eps_max
+        self.steps = steps
+        self.exploration_ratio = exploration_ratio
+
+        self.exploration_steps = int(steps * self.exploration_ratio)
+
+    def __call__(self, step):
+        if step < self.exploration_steps:
+            return self.eps_max
+
+        decay_steps = self.steps - self.exploration_steps
+        decay_rate = (self.eps_max - self.eps_min) / decay_steps
+
+        epsilon = self.eps_max - (step - self.exploration_steps) * decay_rate
+
+        return max(self.eps_min, epsilon)
+
+class DQNActor:
+
+    def __init__(self):
+        pass
--- modules/networks.py
+++ modules/networks.py
@@ -10,6 +10,8 @@ Neural Network declarations, to be used by actors.
 """
 
 # IMPORTS
+import torch
+
 from torch import nn
 
 # CONSTANTS
@@ -20,13 +22,63 @@ from torch import nn
 class DQN(nn.Module):
     """
     Vanilla NN for predicting action, given current observation state.
+    DQN credit:
+        https://www.kaggle.com/code/shreydan/deep-q-learning-playing-atari-breakout#Double-DQN-(DDQN)
+        |-> in this tutorial, we convert to grayscale, and pass 4 frames into a CNN.
+        |-> thus, one kernel per channel, summed together for each channel after one convolution.
     """
 
-    def __init__(self):
+    def __init__(self, stacked_gray_frames: int = 4, action_space: int = 4):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Linear(512, 4)
+            nn.Conv2d(stacked_gray_frames, 32, 8, 4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 4, 2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3),
+            nn.ReLU(),
+            # This step wasn't included in the original article.
+            nn.Flatten(),
+            nn.Linear(3136, 512),
+            nn.ReLU(),
+            nn.Linear(512, action_space)
         )
 
     def forward(self, x):
         return self.model(x)
+
+class PPONetwork(nn.Module):
+    """
+    NN for doing PPO, actor-critic style.
+
+    Actors select the best action, given the current observation.
+
+    Critics estimate the value of the action taken at the current observation.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.base = nn.Sequential(
+            nn.Conv2d(3, 32, 8, 4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 4, 2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(3136, 512),
+            nn.ReLU()
+        )
+        self.actor = nn.Sequential(
+            nn.Linear(512, 4)
+        )
+        self.critic = nn.Sequential(
+            nn.Linear(512, 1)
+        )
+
+    def forward(self, x, action=None):
+        x = self.base(x)
+        probs = torch.distributions.Categorical(logits=self.actor(x))
+        if action is None:
+            action = probs.sample()
+        return action, probs.log_prob(action), probs.entropy(), self.critic(x)
